b372cd0d6b45526aad89efbfcc8c21b060e06b1a
Laith Alrawe <laith261@gmail.com>
2023-01-21 18:28:35 +0300

    add singin

--- API.php
+++ API.php
@@ -7,34 +7,34 @@ header("Accept: application/json");
 $_POST = json_decode(file_get_contents('php://input'), true);
 // print_r($_POST);
 if (isset($_POST["singup"])) {
-    $singup = $_POST["singup"];
-    $refeare_code = isset($singup["refeare"]) ? $singup["refeare"] : null;
+    $data = $_POST["singup"];
+    $refeare_code = isset($data["refeare"]) ? $data["refeare"] : null;
     //
-    if (Validtion::allFill($singup)) {
+    if (Validtion::allFill($data)) {
         json(array("status" => false, "message" => "Fill All Requiered Inputs"));
     }
-    if ($qq->isInDb("users", array("phone" => $singup["phone"]))) {
+    if ($qq->isInDb("users", array("phone" => $data["phone"]))) {
         json(array("status" => false, "message" => "Phone All Ready Used"));
     }
     //
-    if (empty($singup["name"]) || !preg_match("/^[a-zA-Z\s]+$/", $singup["name"])) {
+    if (empty($data["name"]) || !preg_match("/^[a-zA-Z\s]+$/", $data["name"])) {
         json(array("status" => false, "message" => "Name is Invalid"));
     }
     //
-    if (Validtion::Phone($singup["phone"])) {
-        json(array("status" => false, "message" => "Not Valid Email"));
+    if (Validtion::Phone($data["phone"])) {
+        json(array("status" => false, "message" => "Not Valid Phone Number"));
     }
     //
-    if (Validtion::Password($singup["password"])) {
+    if (Validtion::Password($data["password"])) {
         json(array("status" => false, "message" => "Not Valid Password Must Be More Then 5 char"));
     }
     //
-    if (isset($singup["refeare"])) {
-        $refeareCount = $qq->nums($qq->select("users", array("refeare_code" => $singup["refeare"]))) == 0;
+    if (isset($data["refeare"])) {
+        $refeareCount = $qq->nums($qq->select("users", array("refeare_code" => $data["refeare"]))) == 0;
         if ($refeareCount) {
             json(array("status" => false, "message" => "Not Valid Refeare Code"));
         }
-        unset($singup["refeare"]);
+        unset($data["refeare"]);
     }
     //
     $refeare = "";
@@ -42,10 +42,10 @@ if (isset($_POST["singup"])) {
         $refeare = uniqid();
     } while ($qq->nums($qq->select("users", array("refeare_code" => $refeare))) > 0);
     //
-    $singup["refeare_code"] = $refeare;
-    $singup["password"] = password_hash($singup["password"], PASSWORD_DEFAULT);
+    $data["refeare_code"] = $refeare;
+    $data["password"] = password_hash($data["password"], PASSWORD_DEFAULT);
     //
-    $query = $qq->insert("users", $singup);
+    $query = $qq->insert("users", $data);
     $status = $query == true;
     if ($refeare_code != null && $status) {
         $re = $qq->last();
@@ -57,4 +57,34 @@ if (isset($_POST["singup"])) {
         }
     }
     json(array("status" => $status));
+}
+
+if (isset($_POST["singin"])) {
+    $data = $_POST["singin"];
+     //
+     if (Validtion::allFill($data)) {
+        json(array("status" => false, "message" => "Fill All Requiered Inputs"));
+    }
+    //
+    if (Validtion::Phone($data["phone"])) {
+        json(array("status" => false, "message" => "Not Valid Phone Number"));
+    }
+    //
+    if (Validtion::Password($data["password"])) {
+        json(array("status" => false, "message" => "Not Valid Password Must Be More Then 5 char"));
+    }
+    $query=$qq->select("users",array("phone"=>$data["phone"]));
+    if($qq->nums($query)==0){
+        json(array("status" => false, "message" => "Incorrect Information 1"));
+    }
+    $fetch=$qq->fetch($query);
+    $password=$fetch["password"];
+    
+    if(!password_verify($data["password"],$password )){
+        json(array("status" => false, "message" => "Incorrect Information 2"));
+    }
+    json(array("status"=>true,"data"=>array(
+        "id"=>$fetch["uid"],
+        "name"=>$fetch["name"],
+    )));
 }
